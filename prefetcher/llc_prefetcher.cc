5ac66c14273fcc77f4da41aafa4304a863d86a76
sethpugsley <sethpugsley@gmail.com>
2019-01-18 14:29:04 -0800

    added LLC sample prefetchers

---
+++ prefetcher/llc_prefetcher.cc
@@ -0,0 +1,21 @@
+#include "cache.h"
+
+void CACHE::llc_prefetcher_initialize() 
+{
+
+}
+
+void CACHE::llc_prefetcher_operate(uint32_t cpu, uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type)
+{
+
+}
+
+void CACHE::llc_prefetcher_cache_fill(uint32_t cpu, uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr)
+{
+
+}
+
+void CACHE::llc_prefetcher_final_stats()
+{
+
+}
