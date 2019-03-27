fa331789e4b665ba2c8a9697caca1d4d908fc443
Alexandre V. Jamet <alexandre.jamet@bsc.es>
2019-03-27 11:11:00 +0100

    Using CMake as a modular build system for the CPU Simulator.
    Note: with reespect to the previous implementation of the build
    system based on bash scripts, this new implementation is either
    imcomplete and not tested on multi-core builds.

---
+++ src/replacement/lru/lru.cmake
@@ -0,0 +1,7 @@
+file(
+  GLOB_RECURSE
+  CHAMPSIM_REPLACEMENT_POLICY_SOURCES
+  ${CMAKE_CURRENT_SOURCE_DIR}/src/replacement/lru/*.cc
+)
+
+list(APPEND CHAMPSIM_BRANCH_PREDICTOR_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/src/replacement/base_replacement.cc)
