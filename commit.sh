e394390273fbf35040901a41864eecd72d10d948
Nicolau Alff <nicolau.alff@gmail.com>
2019-12-30 03:43:29 -0300

    correção

--- commit.sh
+++ commit.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 
 
-
 if [[ -z "$1" ]]; then
   echo "Entrada vazia"
   echo "Padrão de chamada: ./commit <mensagem do commit>"
 elif [[ -n "$1" ]]; then
-    MENSAGEM= $1
     git add *
-    git commit -m $MENSAGEM
-    git push origin alff
+    git commit -m $1
+    git push origin Alff
 fi
