c554253495c2b4bf195465a0cd6b875c24bd2d64
sethpugsley <sethpugsley@gmail.com>
2020-09-04 15:58:01 -0700

    Virtual Memory overhaul, part 1 (#60)
    * replaced va_to_pa with VirtualMemory class
    
    * adding remaining file edits for VirtualMemory class
    
    * Made lg2() constexpr and removed extern from vmem.h
    
    * removed VirtualMemory deconstructor, and changed vmem to not be a pointer
    
    Co-authored-by: Nathan Gober <ngober@protonmail.com>

--- src/ooo_cpu.cc
+++ src/ooo_cpu.cc
@@ -2,11 +2,14 @@
 
 #include "ooo_cpu.h"
 #include "set.h"
+#include "vmem.h"
 
 // out-of-order core
 O3_CPU ooo_cpu[NUM_CPUS]; 
 uint64_t current_core_cycle[NUM_CPUS], stall_cycle[NUM_CPUS];
 uint32_t SCHEDULING_LATENCY = 0, EXEC_LATENCY = 0, DECODE_LATENCY = 0;
+
+extern VirtualMemory vmem;
 
 void O3_CPU::initialize_core()
 {
@@ -430,7 +433,7 @@ uint32_t O3_CPU::add_to_ifetch_buffer(ooo_model_instr *arch_instr)
   IFETCH_BUFFER.entry[index].event_cycle = current_core_cycle[cpu];
 
   // magically translate instructions
-  uint64_t instr_pa = va_to_pa(cpu, IFETCH_BUFFER.entry[index].instr_id, IFETCH_BUFFER.entry[index].ip , (IFETCH_BUFFER.entry[index].ip)>>LOG2_PAGE_SIZE, 1);
+  uint64_t instr_pa = vmem.va_to_pa(cpu, IFETCH_BUFFER.entry[index].ip);
   instr_pa >>= LOG2_PAGE_SIZE;
   instr_pa <<= LOG2_PAGE_SIZE;
   instr_pa |= (IFETCH_BUFFER.entry[index].ip & ((1 << LOG2_PAGE_SIZE) - 1));  
@@ -740,7 +743,7 @@ int O3_CPU::prefetch_code_line(uint64_t pf_v_addr)
   if (L1I.PQ.occupancy < L1I.PQ.SIZE)
     {
       // magically translate prefetches
-      uint64_t pf_pa = (va_to_pa(cpu, 0, pf_v_addr, pf_v_addr>>LOG2_PAGE_SIZE, 1) & (~((1 << LOG2_PAGE_SIZE) - 1))) | (pf_v_addr & ((1 << LOG2_PAGE_SIZE) - 1));
+      uint64_t pf_pa = (vmem.va_to_pa(cpu, pf_v_addr) & (~((1 << LOG2_PAGE_SIZE) - 1))) | (pf_v_addr & ((1 << LOG2_PAGE_SIZE) - 1));
 
       PACKET pf_packet;
       pf_packet.instruction = 1; // this is a code prefetch
