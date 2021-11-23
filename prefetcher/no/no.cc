03f24c0cd7a7ef0278e5e96061695e69fa3f899f
Nathan Gober <ngober@protonmail.com>
2021-11-23 15:52:24 -0600

    Removed print from 'no' prefetcher

--- prefetcher/no/no.cc
+++ prefetcher/no/no.cc
@@ -1,7 +1,5 @@
 #include "cache.h"
 
-#include <iostream>
-
 void CACHE::prefetcher_initialize()
 {
 }
@@ -13,7 +11,6 @@ uint32_t CACHE::prefetcher_cache_operate(uint64_t addr, uint64_t ip, uint8_t cac
 
 uint32_t CACHE::prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr, uint32_t metadata_in)
 {
-    std::cout << addr << std::endl;
     return metadata_in;
 }
 
