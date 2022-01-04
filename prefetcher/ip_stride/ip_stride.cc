4a24af86950e09a5717e4d6508fc666fdbb1daf3
Nathan Gober <ngober@protonmail.com>
2022-01-04 16:07:55 -0600

    Separated out cycle_operate functionality

--- prefetcher/ip_stride/ip_stride.cc
+++ prefetcher/ip_stride/ip_stride.cc
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <array>
+#include <map>
 
 #include "cache.h"
 
@@ -15,11 +16,35 @@ struct tracker_entry
 
 constexpr std::size_t TRACKER_SETS = 256;
 constexpr std::size_t TRACKER_WAYS = 4;
-std::array<tracker_entry, TRACKER_SETS*TRACKER_WAYS> trackers;
+std::map<CACHE*, std::tuple<uint64_t, int64_t, int>> lookahead; // pf_address, stride, degree
+std::map<CACHE*, std::array<tracker_entry, TRACKER_SETS*TRACKER_WAYS>> trackers;
 
 void CACHE::l2c_prefetcher_initialize()
 {
     std::cout << this->NAME << " IP-based stride prefetcher" << std::endl;
+}
+
+void CACHE::prefetcher_cycle_operate()
+{
+    // If a lookahead is active
+    if (auto [pf_address, stride, degree] = lookahead[this]; pf_address != 0)
+    {
+        // If the next step would exceed the degree or run off the page, stop
+        if (degree < PREFETCH_DEGREE && (virtual_prefetch || ((pf_address + (stride << LOG2_BLOCK_SIZE)) >> LOG2_PAGE_SIZE) == (pf_address >> LOG2_PAGE_SIZE)))
+        {
+            // calculate prefetch address
+            pf_address += stride << LOG2_BLOCK_SIZE;
+
+            // check the MSHR occupancy to decide if we're going to prefetch to the L2 or LLC
+            prefetch_line(0, 0, pf_address, (get_occupancy(0,pf_address) < get_size(0,pf_address)/2) ? FILL_L2 : FILL_LLC, 0);
+        }
+        else
+        {
+            pf_address = 0;
+        }
+
+        lookahead[this] = {pf_address, stride, degree+1};
+    }
 }
 
 uint32_t CACHE::prefetcher_cache_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type, uint32_t metadata_in)
@@ -28,7 +53,7 @@ uint32_t CACHE::prefetcher_cache_operate(uint64_t addr, uint64_t ip, uint8_t cac
     int64_t stride = 0;
 
     // get boundaries of tracking set
-    auto set_begin = std::next(std::begin(trackers), ip % TRACKER_SETS);
+    auto set_begin = std::next(std::begin(trackers[this]), ip % TRACKER_SETS);
     auto set_end   = std::next(set_begin, TRACKER_WAYS);
 
     // find the current ip within the set
@@ -41,23 +66,10 @@ uint32_t CACHE::prefetcher_cache_operate(uint64_t addr, uint64_t ip, uint8_t cac
         // no need to check for overflow since these values are downshifted
         stride = (int64_t)cl_addr - (int64_t)found->last_cl_addr;
 
-        // don't do anything if we somehow saw the same address twice in a row
+        // Initialize prefetch state unless we somehow saw the same address twice in a row
         // or if this is the first time we've seen this stride
         if (stride != 0 && stride == found->last_stride)
-        {
-            for (auto i = 0; i<PREFETCH_DEGREE; i++)
-            {
-                // calculate prefetch address
-                uint64_t pf_address = (cl_addr + (stride*(i+1))) << LOG2_BLOCK_SIZE;
-
-                // stop prefetching if we have iterated off the virtual page
-                if (!virtual_prefetch && (pf_address >> LOG2_PAGE_SIZE) != (addr >> LOG2_PAGE_SIZE))
-                    break;
-
-                // check the MSHR occupancy to decide if we're going to prefetch to the L2 or LLC
-                prefetch_line(ip, addr, pf_address, (get_occupancy(0,pf_address) < get_size(0,pf_address)/2) ? FILL_L2 : FILL_LLC, 0);
-            }
-        }
+            lookahead[this] = {cl_addr << LOG2_BLOCK_SIZE, stride, 0};
     }
     else
     {
@@ -74,11 +86,7 @@ uint32_t CACHE::prefetcher_cache_operate(uint64_t addr, uint64_t ip, uint8_t cac
 
 uint32_t CACHE::prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr, uint32_t metadata_in)
 {
-  return metadata_in;
-}
-
-void CACHE::prefetcher_cycle_operate()
-{
+    return metadata_in;
 }
 
 void CACHE::prefetcher_final_stats()
