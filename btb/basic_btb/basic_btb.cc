d203e3cc76df202883cbbe07250a7cdfe86969de
Nathan Gober <ngober@protonmail.com>
2022-01-15 09:43:24 -0600

    Refactored Indirect BTB

--- btb/basic_btb/basic_btb.cc
+++ btb/basic_btb/basic_btb.cc
@@ -9,10 +9,13 @@
 #include "ooo_cpu.h"
 
 #include <algorithm>
+#include <bitset>
+
+#include "util.h"
 
 constexpr std::size_t BASIC_BTB_SETS = 1024;
 constexpr std::size_t BASIC_BTB_WAYS = 8;
-#define BASIC_BTB_INDIRECT_SIZE 4096
+constexpr std::size_t BASIC_BTB_INDIRECT_SIZE = 4096;
 #define BASIC_BTB_RAS_SIZE 64
 #define BASIC_BTB_CALL_INSTR_SIZE_TRACKERS 1024
 
@@ -26,8 +29,8 @@ struct BASIC_BTB_ENTRY
 
 std::map<O3_CPU*, std::array<BASIC_BTB_ENTRY, BASIC_BTB_SETS*BASIC_BTB_WAYS>> basic_btb;
 
-uint64_t basic_btb_indirect[NUM_CPUS][BASIC_BTB_INDIRECT_SIZE];
-uint64_t basic_btb_conditional_history[NUM_CPUS];
+std::map<O3_CPU*, std::array<uint64_t, BASIC_BTB_INDIRECT_SIZE>> basic_btb_indirect;
+std::map<O3_CPU*, std::bitset<lg2(BASIC_BTB_INDIRECT_SIZE)>> basic_btb_conditional_history;
 
 uint64_t basic_btb_ras[NUM_CPUS][BASIC_BTB_RAS_SIZE];
 int basic_btb_ras_index[NUM_CPUS];
@@ -47,11 +50,6 @@ uint64_t basic_btb_abs_addr_dist(uint64_t addr1, uint64_t addr2) {
   }
 
   return addr2 - addr1;
-}
-
-uint64_t basic_btb_indirect_hash(uint8_t cpu, uint64_t ip) {
-  uint64_t hash = (ip >> 2) ^ (basic_btb_conditional_history[cpu]);
-  return (hash & (BASIC_BTB_INDIRECT_SIZE-1));
 }
 
 void push_basic_btb_ras(uint8_t cpu, uint64_t ip) {
@@ -97,10 +95,8 @@ void O3_CPU::initialize_btb() {
 
   std::fill(std::begin(basic_btb[this]), std::end(basic_btb[this]), BASIC_BTB_ENTRY{});
 
-  for (uint32_t i = 0; i < BASIC_BTB_INDIRECT_SIZE; i++) {
-    basic_btb_indirect[cpu][i] = 0;
-  }
-  basic_btb_conditional_history[cpu] = 0;
+  std::fill(std::begin(basic_btb_indirect[this]), std::end(basic_btb_indirect[this]), 0);
+  basic_btb_conditional_history[this] = 0;
 
   for (uint32_t i = 0; i < BASIC_BTB_RAS_SIZE; i++) {
     basic_btb_ras[cpu][i] = 0;
@@ -130,10 +126,13 @@ std::pair<uint64_t, uint8_t> O3_CPU::btb_prediction(uint64_t ip, uint8_t branch_
     target += basic_btb_get_call_size(cpu, target);
 
     return std::make_pair(target, always_taken);
-  } else if ((branch_type == BRANCH_INDIRECT) ||
-             (branch_type == BRANCH_INDIRECT_CALL)) {
-    return std::make_pair(basic_btb_indirect[cpu][basic_btb_indirect_hash(cpu, ip)], always_taken);
-  } else {
+  }
+  else if ((branch_type == BRANCH_INDIRECT) || (branch_type == BRANCH_INDIRECT_CALL))
+  {
+      uint64_t hash = (ip >> 2) ^ basic_btb_conditional_history[this].to_ullong();
+      return {basic_btb_indirect[this][hash % std::size(basic_btb_indirect[this])], always_taken};
+  }
+  else {
       // use BTB for all other branches + direct calls
       auto set_idx = (ip >> 2) % BASIC_BTB_SETS;
       auto set_begin = std::next(std::begin(basic_btb[this]), set_idx*BASIC_BTB_WAYS);
@@ -158,15 +157,16 @@ std::pair<uint64_t, uint8_t> O3_CPU::btb_prediction(uint64_t ip, uint8_t branch_
 void O3_CPU::update_btb(uint64_t ip, uint64_t branch_target, uint8_t taken,
                         uint8_t branch_type) {
   // updates for indirect branches
-  if ((branch_type == BRANCH_INDIRECT) ||
-      (branch_type == BRANCH_INDIRECT_CALL)) {
-    basic_btb_indirect[cpu][basic_btb_indirect_hash(cpu, ip)] = branch_target;
+  if ((branch_type == BRANCH_INDIRECT) || (branch_type == BRANCH_INDIRECT_CALL))
+  {
+      uint64_t hash = (ip >> 2) ^ basic_btb_conditional_history[this].to_ullong();
+      basic_btb_indirect[this][hash % std::size(basic_btb_indirect[this])] = branch_target;
   }
-  if (branch_type == BRANCH_CONDITIONAL) {
-    basic_btb_conditional_history[cpu] <<= 1;
-    if (taken) {
-      basic_btb_conditional_history[cpu] |= 1;
-    }
+
+  if (branch_type == BRANCH_CONDITIONAL)
+  {
+      basic_btb_conditional_history[this] <<= 1;
+      basic_btb_conditional_history[this].set(0, taken);
   }
 
   if (branch_type == BRANCH_RETURN) {
