d245e93d2ffdb819dcb0ce9217e2c6af8b34a0ec
Nathan Gober <ngober@protonmail.com>
2022-01-18 14:26:25 -0600

    Removed assumption of 2-bit counters

--- branch/gshare/gshare.cc
+++ branch/gshare/gshare.cc
@@ -13,12 +13,13 @@ std::map<O3_CPU*, std::array<int, GS_HISTORY_TABLE_SIZE>> gs_history_table;
 
 constexpr int COUNTER_MAX = (1 << COUNTER_BITS) - 1;
 constexpr int COUNTER_MIN = 0;
+constexpr int COUNTER_THRESH = (COUNTER_MAX + COUNTER_MIN + 1)/2;
 
 void O3_CPU::initialize_branch_predictor()
 {
     std::cout << "CPU " << cpu << " GSHARE branch predictor" << std::endl;
 
-    std::fill(std::begin(gs_history_table[this]), std::end(gs_history_table[this]), (COUNTER_MAX/2)+1); // weakly taken
+    std::fill(std::begin(gs_history_table[this]), std::end(gs_history_table[this]), COUNTER_THRESH); // weakly taken
 }
 
 std::size_t gs_table_hash(uint64_t ip, std::bitset<GLOBAL_HISTORY_LENGTH> bh_vector)
@@ -34,7 +35,7 @@ std::size_t gs_table_hash(uint64_t ip, std::bitset<GLOBAL_HISTORY_LENGTH> bh_vec
 uint8_t O3_CPU::predict_branch(uint64_t ip, uint64_t predicted_target, uint8_t always_taken, uint8_t branch_type)
 {
     auto gs_hash = gs_table_hash(ip, branch_history_vector[this]);
-    return gs_history_table[cpu][gs_hash] >= 2;
+    return gs_history_table[cpu][gs_hash] >= COUNTER_THRESH;
 }
 
 void O3_CPU::last_branch_result(uint64_t ip, uint64_t branch_target, uint8_t taken, uint8_t branch_type)
