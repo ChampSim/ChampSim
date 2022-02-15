cdf6aafc559af293021e2edcfb19d8d331fb3a6a
Nathan Gober <ngober@protonmail.com>
2022-02-15 11:29:23 -0600

    Added invariant note for instruction prefetchers

--- prefetcher/next_line_instr/next_line.cc
+++ prefetcher/next_line_instr/next_line.cc
@@ -8,6 +8,7 @@ void CACHE::prefetcher_branch_operate(uint64_t ip, uint8_t branch_type, uint64_t
 
 uint32_t CACHE::prefetcher_cache_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type, uint32_t metadata_in)
 {
+  assert(addr == ip); // Invariant for instruction prefetchers
   uint64_t pf_addr = addr + (1 << LOG2_BLOCK_SIZE);
   prefetch_line(pf_addr, true, metadata_in);
   return metadata_in;
