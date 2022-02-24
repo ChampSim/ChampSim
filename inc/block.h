b11039306373e028ec6add4e3a45551fefe7a564
Nathan Gober <ngober@protonmail.com>
2022-02-23 19:24:20 -0600

    Moved LSQ_ENTRY to inc/ooo_cpu.h

--- inc/block.h
+++ inc/block.h
@@ -9,7 +9,6 @@
 #include "instruction.h"
 
 class MemoryRequestProducer;
-class LSQ_ENTRY;
 
 // message packet
 class PACKET
@@ -45,25 +44,5 @@ void packet_dep_merge(LIST& dest, LIST& src)
   auto uniq_end = std::unique(std::begin(dest), std::end(dest));
   dest.erase(uniq_end, std::end(dest));
 }
-
-// load/store queue
-struct LSQ_ENTRY
-{
-    bool valid = false;
-    uint64_t instr_id = 0;
-    uint64_t virtual_address = 0;
-    uint64_t ip = 0;
-    uint64_t event_cycle = 0;
-
-    champsim::circular_buffer<ooo_model_instr>::iterator rob_index;
-
-    uint8_t asid[2] = {std::numeric_limits<uint8_t>::max(), std::numeric_limits<uint8_t>::max()};
-    bool translate_issued = false;
-    bool fetch_issued = false;
-    bool complete = false;
-
-    uint64_t physical_address = 0;
-    uint64_t producer_id = std::numeric_limits<uint64_t>::max();
-};
 
 #endif
--- inc/ooo_cpu.h
+++ inc/ooo_cpu.h
@@ -30,6 +30,25 @@ class CacheBus : public MemoryRequestProducer
         void return_data(PACKET *packet);
 };
 
+struct LSQ_ENTRY
+{
+  bool valid = false;
+  uint64_t instr_id = 0;
+  uint64_t virtual_address = 0;
+  uint64_t ip = 0;
+  uint64_t event_cycle = 0;
+
+  champsim::circular_buffer<ooo_model_instr>::iterator rob_index;
+
+  uint8_t asid[2] = {std::numeric_limits<uint8_t>::max(), std::numeric_limits<uint8_t>::max()};
+  bool translate_issued = false;
+  bool fetch_issued = false;
+  bool complete = false;
+
+  uint64_t physical_address = 0;
+  uint64_t producer_id = std::numeric_limits<uint64_t>::max();
+};
+
 // cpu
 class O3_CPU : public champsim::operable
 {
@@ -65,6 +84,7 @@ public:
   champsim::circular_buffer<ooo_model_instr> DISPATCH_BUFFER;
   champsim::delay_queue<ooo_model_instr> DECODE_BUFFER;
   champsim::circular_buffer<ooo_model_instr> ROB;
+
   std::vector<LSQ_ENTRY> LQ;
   std::vector<LSQ_ENTRY> SQ;
 
