de392dc391b39979c2a435e9f3eaa672b743f255
Nathan Gober <ngober@protonmail.com>
2022-07-20 16:48:38 -0500

    Update 601-multiple-inflight-walks.cc

--- test/601-multiple-inflight-walks.cc
+++ test/601-multiple-inflight-walks.cc
@@ -11,7 +11,7 @@
 SCENARIO("A page table walker can handle multiple concurrent walks") {
   GIVEN("A 5-level virtual memory") {
     constexpr std::size_t levels = 5;
-    MEMORY_CONTROLLER dram{1};
+    MEMORY_CONTROLLER dram{1, 3200, 12.5, 12.5, 12.5, 7.5};
     VirtualMemory vmem{20, 1<<12, levels, 200, dram};
     do_nothing_MRC mock_ll{5};
     PageTableWalker uut{"600-uut-0", 0, {{1,1,0}, {1,1,0}, {1,1,0}, {1,1,0}}, 2, 2, 1, 1, 1, &mock_ll, vmem};
