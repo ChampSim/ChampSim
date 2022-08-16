6efdbaf3f74cc7d3e4c79b54ee04fa2905f0b252
Nathan Gober <ngober@protonmail.com>
2022-08-16 16:54:20 -0500

    Fixed make test

--- Makefile
+++ Makefile
@@ -6,39 +6,54 @@ CXXFLAGS += --std=c++17 -Wall -O3
 all: all_execs
 
 cppsrc = $(wildcard src/*.cc)
+testsrc = $(wildcard test/*.cc)
+
+$(cppsrc:.cc=.o) $(testsrc:.cc=.o): CPPFLAGS += -MMD -MP
 
 # Generated configuration makefile contains:
-#  - Each module's compilation flags
-#  - Each module's source files, appended to $(cppsrc) and $(csrc)
-#  - $(executable_name)
+#  - $(executable_name), the list of all executables in the configuration
+#  - $(build_dirs), the list of all directories that hold executables
+#  - $(module_dirs), the list of all directories that hold module object files
+#  - $(module_objs), the list of all object files corresponding to modules
+#  - All dependencies and flags assigned according to the modules
 include _configuration.mk
 
-all_execs: $(executable_name)
+all_execs: $(filter-out test/000-test-main, $(executable_name))
 
+# Remove all intermediate files
 clean:
-	find src test $(objdir) \( -name '*.o' -o -name '*.d' \) -delete
-	$(RM) test/000-test-main
+	@-find src test $(module_dirs) \( -name '*.o' -o -name '*.d' \) -delete &> /dev/null
+	@-$(RM) test/000-test-main
 
+# Remove all configuration files
 configclean: clean
-	$(RM) -r $(objdir) _configuration.mk
+	@-$(RM) -r $(module_dirs) _configuration.mk
 
-$(sort $(required_dirs)): | $(dir $@)
+# Make directories that don't exist
+# exclude "test" to not conflict with the phony target
+$(filter-out test, $(sort $(build_dirs) $(module_dirs))): | $(dir $@)
 	-mkdir $@
 
-$(cppsrc:.cc=.o): CPPFLAGS += -MMD -MP
-
+# All module .o files should be made like .cc files
 $(module_objs):
 	$(COMPILE.cc) $(OUTPUT_OPTION) $<
 
-$(executable_name): $(cppsrc:.cc=.o) | $(bindir)
-	$(LINK.cc) $(OUTPUT_OPTION) $^
+# Add main as a dependency for the primary executables
+$(filter-out test/000-test-main, $(executable_name)): src/main.o
 
-test/000-test-main: CXXFLAGS += -fsanitize=address -fno-omit-frame-pointer
-test/000-test-main: $(filter-out src/main.o, $(cppsrc:.cc=.o)) $(patsubst %.cc,%.o,$(wildcard test/*.cc))
-	$(LINK.cc) $(OUTPUT_OPTION) $^
+# Add address sanitizers for tests
+#test/000-test-main: CXXFLAGS += -fsanitize=address -fno-omit-frame-pointer
 
+# Test depends on the sources in test/
+test/000-test-main: $(testsrc:.cc=.o)
+
+# Tests: build and run
 test: test/000-test-main
 	test/000-test-main
+
+# Both main and test executables depend on the sources in src/ (except for main)
+$(executable_name): $(filter-out src/main.o, $(cppsrc:.cc=.o))
+	$(LINK.cc) $(OUTPUT_OPTION) $^
 
 -include $(wildcard src/*.d) $(wildcard test/*.d) $(foreach dir,$(wildcard .csconfig/*/),$(wildcard $(dir)/*.d))
 
--- config/makefile.py
+++ config/makefile.py
@@ -29,7 +29,8 @@ def executable_opts(obj_dir, build_id, executable, config_file):
             retval += '{}: {} += {}\n'.format(executable, k, config_file[k])
 
     retval += executable + ': CPPFLAGS += -I' + os.path.join(dest_dir, 'inc') + '\n'
-    retval += 'required_dirs += ' + ' '.join(generate_dirs(os.path.split(executable)[0])) + '\n'
+    retval += executable + ': | ' + ' '.join(generate_dirs(os.path.split(executable)[0])) + '\n'
+    retval += 'build_dirs += ' + ' '.join(generate_dirs(os.path.split(executable)[0])) + '\n'
     return retval
 
 def module_opts(source_dir, obj_dir, build_id, name, opts, exe):
@@ -44,11 +45,15 @@ def module_opts(source_dir, obj_dir, build_id, name, opts, exe):
     retval += '# Destination: ' + dest_dir + '\n'
     retval += '###\n\n'
 
-    retval += dir_varname + ' = ' + ' '.join(generate_dirs(dest_dir)) + '\n'
+    obj_dirnames = [dest_dir]
+    obj_filenames = []
     for base,dirs,files in os.walk(source_dir):
-        for f,ext in map(os.path.splitext, files):
-            if ext in ('.cc',):
-                retval += obj_varname + ' += ' + os.path.join(dest_dir,f) + '.o\n'
+        obj_dirnames.extend(os.path.join(base, d) for d in dirs)
+        obj_filenames.extend(os.path.join(dest_dir, os.path.relpath(base, source_dir), f)+'.o' for f,ext in map(os.path.splitext, files) if ext in ('.cc',))
+
+    retval += '{} = {}\n'.format(dir_varname, ' '.join(obj_dirnames))
+    for f in obj_filenames:
+        retval += '{} += {}\n'.format(obj_varname, f)
 
     retval += '$({}): | $({})\n'.format(obj_varname, dir_varname)
     retval += '$({}): CPPFLAGS += -I{}\n'.format(obj_varname, source_dir)
@@ -60,18 +65,14 @@ def module_opts(source_dir, obj_dir, build_id, name, opts, exe):
 
     retval += exe + ': $(' + obj_varname + ')\n'
     retval += 'module_objs += $(' + obj_varname + ')\n'
-    retval += 'required_dirs += $(' + dir_varname + ')\n'
+    retval += 'module_dirs += $(' + dir_varname + ')\n'
 
     return retval
 
-def get_makefile_string(build_id, module_info, **config_file):
-
-    name = config_file.get('name')
-    executable = '$(bindir)/' + config_file.get('executable_name', 'champsim' + ('' if name is None else '_'+name))
-
-    retval = executable_opts('$(objdir)', build_id, executable, config_file)
+def get_makefile_string(objdir, build_id, executable, module_info, config_file):
+    retval = executable_opts(objdir, build_id, executable, config_file)
     retval += '\n'
-    retval += '\n'.join(module_opts(v['fname'], '$(objdir)', build_id, k, v['opts'], executable) for k,v in module_info.items())
+    retval += '\n'.join(module_opts(v['fname'], objdir, build_id, k, v['opts'], executable) for k,v in module_info.items())
 
     return retval
 
