e08a471f5b4cad524ad63f1259ac32c09744f906
Nathan Gober <ngober@protonmail.com>
2022-11-09 11:11:59 -0600

    Moved bit functions into msl namespace

---
+++ inc/msl/bits.h
@@ -0,0 +1,13 @@
+#ifndef MSL_BITS_H
+#define MSL_BITS_H
+
+namespace champsim::msl
+{
+  constexpr unsigned lg2(uint64_t n) { return n < 2 ? 0 : 1 + lg2(n / 2); }
+
+  constexpr uint64_t bitmask(std::size_t begin, std::size_t end = 0) { return ((1ull << (begin - end)) - 1) << end; }
+
+  constexpr uint64_t splice_bits(uint64_t upper, uint64_t lower, std::size_t bits) { return (upper & ~bitmask(bits)) | (lower & bitmask(bits)); }
+}
+
+#endif
--- inc/util.h
+++ inc/util.h
@@ -6,14 +6,7 @@
 #include <optional>
 #include <vector>
 
-namespace champsim
-{
-  constexpr unsigned lg2(uint64_t n) { return n < 2 ? 0 : 1 + lg2(n / 2); }
-
-  constexpr uint64_t bitmask(std::size_t begin, std::size_t end = 0) { return ((1ull << (begin - end)) - 1) << end; }
-
-  constexpr uint64_t splice_bits(uint64_t upper, uint64_t lower, std::size_t bits) { return (upper & ~bitmask(bits)) | (lower & bitmask(bits)); }
-}
+#include "msl/bits.h"
 
 template <typename T>
 struct is_valid {
@@ -90,6 +83,9 @@ struct ord_event_cycle {
 
 namespace champsim
 {
+  using msl::lg2;
+  using msl::bitmask;
+  using msl::splice_bits;
 
 template <typename T>
 class simple_lru_table
