67e91678f3f52bc157b1991dd430d127ab881296
Nathan Gober <ngober@protonmail.com>
2022-11-15 14:44:32 -0600

    Updated ip_stride to use MSL

--- prefetcher/ip_stride/ip_stride.cc
+++ prefetcher/ip_stride/ip_stride.cc
@@ -4,27 +4,37 @@
 
 #include "cache.h"
 
+#include "msl/lru_table.h"
+
 namespace
 {
-constexpr int PREFETCH_DEGREE = 3;
+struct tracker
+{
+  struct tracker_entry {
+    uint64_t ip = 0;              // the IP we're tracking
+    uint64_t last_cl_addr = 0;    // the last address accessed by this IP
+    int64_t last_stride = 0;      // the stride between the last two addresses accessed by this IP
 
-struct tracker_entry {
-  uint64_t ip = 0;              // the IP we're tracking
-  uint64_t last_cl_addr = 0;    // the last address accessed by this IP
-  int64_t last_stride = 0;      // the stride between the last two addresses accessed by this IP
-  uint64_t last_used_cycle = 0; // use LRU to evict old IP trackers
+    auto index() const { return ip; }
+    auto tag() const { return ip; }
+  };
+
+  struct lookahead_entry {
+    uint64_t address = 0;
+    int64_t stride = 0;
+    int degree = 0; // degree remaining
+  };
+
+  constexpr static std::size_t TRACKER_SETS = 256;
+  constexpr static std::size_t TRACKER_WAYS = 4;
+  constexpr static int PREFETCH_DEGREE = 3;
+
+  lookahead_entry lookahead{};
+
+  champsim::msl::lru_table<tracker_entry> table{TRACKER_SETS, TRACKER_WAYS};
 };
 
-struct lookahead_entry {
-  uint64_t address = 0;
-  int64_t stride = 0;
-  int degree = 0; // degree remaining
-};
-
-constexpr std::size_t TRACKER_SETS = 256;
-constexpr std::size_t TRACKER_WAYS = 4;
-std::map<CACHE*, lookahead_entry> lookahead;
-std::map<CACHE*, std::array<tracker_entry, TRACKER_SETS * TRACKER_WAYS>> trackers;
+std::map<CACHE*, tracker_entry> trackers;
 } // namespace
 
 void CACHE::prefetcher_initialize() { std::cout << NAME << " IP-based stride prefetcher" << std::endl; }
@@ -32,7 +42,7 @@ void CACHE::prefetcher_initialize() { std::cout << NAME << " IP-based stride pre
 void CACHE::prefetcher_cycle_operate()
 {
   // If a lookahead is active
-  if (auto [old_pf_address, stride, degree] = ::lookahead[this]; degree > 0) {
+  if (auto [old_pf_address, stride, degree] = ::trackers[this].lookahead; degree > 0) {
     auto pf_address = old_pf_address + (stride << LOG2_BLOCK_SIZE);
 
     // If the next step would exceed the degree or run off the page, stop
@@ -41,10 +51,10 @@ void CACHE::prefetcher_cycle_operate()
       // level or not
       bool success = prefetch_line(pf_address, (get_occupancy(0, pf_address) < (get_size(0, pf_address) / 2)), 0);
       if (success)
-        ::lookahead[this] = {pf_address, stride, degree - 1};
+        ::trackers[this].lookahead = {pf_address, stride, degree - 1};
       // If we fail, try again next cycle
     } else {
-      ::lookahead[this] = {};
+      ::trackers[this].lookahead = {};
     }
   }
 }
@@ -54,15 +64,11 @@ uint32_t CACHE::prefetcher_cache_operate(uint64_t addr, uint64_t ip, uint8_t cac
   uint64_t cl_addr = addr >> LOG2_BLOCK_SIZE;
   int64_t stride = 0;
 
-  // get boundaries of tracking set
-  auto set_begin = std::next(std::begin(::trackers[this]), (ip % ::TRACKER_SETS) * ::TRACKER_WAYS);
-  auto set_end = std::next(set_begin, ::TRACKER_WAYS);
-
   // find the current ip within the set
-  auto found = std::find_if(set_begin, set_end, [ip](auto x) { return x.ip == ip; });
+  auto found = ::trackers[this].table.check_hit({ip, cl_addr, stride});
 
   // if we found a matching entry
-  if (found != set_end) {
+  if (found.has_value()) {
     // calculate the stride between the current address and the last address
     // no need to check for overflow since these values are downshifted
     stride = (int64_t)cl_addr - (int64_t)found->last_cl_addr;
@@ -70,14 +76,11 @@ uint32_t CACHE::prefetcher_cache_operate(uint64_t addr, uint64_t ip, uint8_t cac
     // Initialize prefetch state unless we somehow saw the same address twice in
     // a row or if this is the first time we've seen this stride
     if (stride != 0 && stride == found->last_stride)
-      ::lookahead[this] = {cl_addr, stride, ::PREFETCH_DEGREE};
-  } else {
-    // replace by LRU
-    found = std::min_element(set_begin, set_end, [](auto x, auto y) { return x.last_used_cycle < y.last_used_cycle; });
+      ::trackers[this].lookahead = {cl_addr, stride, ::PREFETCH_DEGREE};
   }
 
   // update tracking set
-  *found = {ip, cl_addr, stride, current_cycle};
+  ::trackers[this].table.fill({ip, cl_addr, stride});
 
   return metadata_in;
 }
