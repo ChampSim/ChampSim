0f3c9401caf7d343a59db768d8b406315a919b8f
Nathan Gober <ngober@protonmail.com>
2022-12-12 15:21:02 -0600

    Fixed undefined behavior in test 802

--- test/802-pte-page.cc
+++ test/802-pte-page.cc
@@ -6,7 +6,7 @@
 #include "util/bits.h"
 
 SCENARIO("The virtual memory issues references to blocks within a page if they are in the same level") {
-  constexpr unsigned vmem_size_bits = 33;
+  constexpr unsigned vmem_size_bits = 34;
 
   auto pte_page_size = (1ull << 11);
   auto level = GENERATE(2,3,4);
@@ -23,7 +23,7 @@ SCENARIO("The virtual memory issues references to blocks within a page if they a
       champsim::address req_page{~(champsim::bitmask(size) << shamt)};
       for (std::size_t i = 0; i < size; ++i) {
         given_pages.push_back(uut.get_pte_pa(0, req_page, level).first);
-        req_page += 1 << shamt;
+        req_page += 1ull << shamt;
       }
       std::sort(std::begin(given_pages), std::end(given_pages));
 
@@ -32,9 +32,9 @@ SCENARIO("The virtual memory issues references to blocks within a page if they a
         REQUIRE(given_pages.front().slice_upper(shamt + champsim::lg2(size)) == given_pages.back().slice_upper(shamt + champsim::lg2(size)));
       }
 
-      THEN("The entries are spaced by PTE_BYTES") {
-        REQUIRE(champsim::offset(given_pages.at(0), given_pages.at(1)) == static_cast<champsim::address::difference_type>(PTE_BYTES));
-        REQUIRE(champsim::offset(given_pages.front(), given_pages.back()) == static_cast<champsim::address::difference_type>(PTE_BYTES*(size-1)));
+      THEN("The entries are spaced by pte_page_size") {
+        REQUIRE(champsim::offset(given_pages.at(0), given_pages.at(1)) == static_cast<champsim::address::difference_type>(pte_page_size));
+        REQUIRE(champsim::offset(given_pages.front(), given_pages.back()) == static_cast<champsim::address::difference_type>(pte_page_size*(size-1)));
       }
 
       THEN("No pages are duplicated") {
