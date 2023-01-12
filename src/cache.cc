a5e11e9f21d2d3f973168b1a42460c7ebfe3e0e0
Nathan Gober <ngober@protonmail.com>
2023-01-12 16:45:36 -0600

    Fixed warning

--- src/cache.cc
+++ src/cache.cc
@@ -339,7 +339,7 @@ auto CACHE::get_set_span(uint64_t address) const -> std::pair<std::vector<BLOCK>
   return get_span(std::cbegin(block), static_cast<std::vector<BLOCK>::difference_type>(set_idx), NUM_WAY); // safe cast because of prior assert
 }
 
-uint64_t CACHE::get_way(int address_space, uint64_t address, uint64_t set) const
+uint64_t CACHE::get_way(int address_space, uint64_t address, uint64_t) const
 {
   auto [begin, end] = get_set_span(address);
   return std::distance(begin, std::find_if(begin, end, eq_addr<BLOCK>(address_space, address, OFFSET_BITS)));
