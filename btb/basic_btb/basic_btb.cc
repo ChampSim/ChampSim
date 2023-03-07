37ace3430d37fcd9135c13ccca25e0753b1ce822
Sawan Singh <singh.sawan@um.es>
2023-03-07 11:18:48 +0100

    Fixed thissue 296 which leads to high MPKI

--- btb/basic_btb/basic_btb.cc
+++ btb/basic_btb/basic_btb.cc
@@ -86,7 +86,7 @@ std::pair<uint64_t, uint8_t> O3_CPU::btb_prediction(uint64_t ip)
     return {::INDIRECT_BTB[this][hash % std::size(::INDIRECT_BTB[this])], true};
   }
 
-  return {btb_entry->target, btb_entry->type != ::branch_info::CONDITIONAL};
+  return { btb_entry->target, btb_entry->type != ::branch_info::CONDITIONAL };
 }
 
 void O3_CPU::update_btb(uint64_t ip, uint64_t branch_target, uint8_t taken, uint8_t branch_type)
@@ -130,6 +130,13 @@ void O3_CPU::update_btb(uint64_t ip, uint64_t branch_target, uint8_t taken, uint
     type = ::branch_info::CONDITIONAL;
 
   auto opt_entry = ::BTB.at(this).check_hit({ip, branch_target, type});
+  
+  if (taken) {
+    if (opt_entry->type == ::branch_info::CONDITIONAL && type == ::branch_info::ALWAYS_TAKEN) {
+      type = ::branch_info::CONDITIONAL;
+    }
+  }
+  
   if (opt_entry.has_value()) {
     opt_entry->type = type;
     if (branch_target != 0)
