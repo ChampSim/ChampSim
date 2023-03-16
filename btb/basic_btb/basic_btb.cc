a089b0c2225194266cd740ec7ba2572403ba172f
Alberto Ros <aros@ditec.um.es>
2023-03-16 15:26:22 +0100

    Always taken not used for conditional branches

--- btb/basic_btb/basic_btb.cc
+++ btb/basic_btb/basic_btb.cc
@@ -126,17 +126,11 @@ void O3_CPU::update_btb(uint64_t ip, uint64_t branch_target, uint8_t taken, uint
     type = ::branch_info::INDIRECT;
   else if (branch_type == BRANCH_RETURN)
     type = ::branch_info::RETURN;
-  else if ((branch_target == 0) || !taken)
+  else if (branch_type == BRANCH_CONDITIONAL)
     type = ::branch_info::CONDITIONAL;
 
   auto opt_entry = ::BTB.at(this).check_hit({ip, branch_target, type});
   if (opt_entry.has_value()) {
-    if (taken) {
-      if (opt_entry->type == ::branch_info::CONDITIONAL && type == ::branch_info::ALWAYS_TAKEN) {
-        type = ::branch_info::CONDITIONAL;
-      }
-    }
-
     opt_entry->type = type;
     if (branch_target != 0)
       opt_entry->target = branch_target;
