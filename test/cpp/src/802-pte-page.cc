4f3f4ea322355066028f2acbec3743d2a9906406
Nathan Gober <ngober@protonmail.com>
2023-04-14 15:11:38 -0500

    Fixed bug in test 802

--- test/cpp/src/802-pte-page.cc
+++ test/cpp/src/802-pte-page.cc
@@ -1,9 +1,28 @@
 #include <catch.hpp>
 #include "vmem.h"
 
+#include <cmath>
+
 #include "champsim_constants.h"
 #include "dram_controller.h"
 #include "util.h"
+
+struct AdjDiffMatcher : Catch::Matchers::MatcherGenericBase {
+  uint64_t stride;
+
+  explicit AdjDiffMatcher(uint64_t s) : stride(s) {}
+
+    template<typename Range>
+    bool match(Range const& range) const {
+      std::vector<uint64_t> diffs;
+      std::adjacent_difference(std::cbegin(range), std::cend(range), std::back_inserter(diffs));
+      return std::all_of(std::next(std::cbegin(diffs)), std::cend(diffs), [stride=stride](auto x){ return x == stride; });
+    }
+
+    std::string describe() const override {
+        return "has stride " + std::to_string(stride);
+    }
+};
 
 SCENARIO("The virtual memory issues references to blocks within a page if they are in the same level") {
   auto pte_page_size = (1ull << 11);
@@ -13,31 +32,26 @@ SCENARIO("The virtual memory issues references to blocks within a page if they a
     MEMORY_CONTROLLER dram{1, 3200, 12.5, 12.5, 12.5, 7.5};
     VirtualMemory uut{pte_page_size, 5, 200, dram};
 
-    auto size = PAGE_SIZE / pte_page_size;
-    WHEN("A full set of requests for PTE entries at level " + std::to_string(level) + " are called for") {
-      auto shamt = LOG2_PAGE_SIZE + champsim::lg2(pte_page_size)*level;
-      std::vector<uint64_t> given_pages;
+    uint64_t dist = PAGE_SIZE;
+    for (auto i = 0; i < level; ++i)
+      dist *= pte_page_size;
+    std::vector<uint64_t> req_pages(PAGE_SIZE / pte_page_size, dist);
+    req_pages.front() = 0xcccc000000000000;
+    std::partial_sum(std::cbegin(req_pages), std::cend(req_pages), std::begin(req_pages));
 
-      uint64_t req_page = ~(champsim::bitmask(size) << shamt);
-      for (std::size_t i = 0; i < size; ++i) {
-        given_pages.push_back(uut.get_pte_pa(0, req_page, level).first);
-        req_page += 1 << shamt;
-      }
+    WHEN("A full set of requests for PTE entries at level " + std::to_string(level) + " are called for") {
+      std::vector<uint64_t> given_pages{};
+      std::transform(std::cbegin(req_pages), std::cend(req_pages), std::back_inserter(given_pages), [&](auto req_page){ return uut.get_pte_pa(0, req_page, level).first; });
       std::sort(std::begin(given_pages), std::end(given_pages));
 
       THEN("All entries are on the same page") {
-        REQUIRE((given_pages.at(0) >> (shamt + champsim::lg2(size))) == (given_pages.at(1) >> (shamt + champsim::lg2(size))));
-        REQUIRE((given_pages.front() >> (shamt + champsim::lg2(size))) == (given_pages.back() >> (shamt + champsim::lg2(size))));
+        std::vector<uint64_t> pages;
+        std::transform(std::cbegin(given_pages), std::cend(given_pages), std::back_inserter(pages), [](auto x){ return x >> LOG2_PAGE_SIZE; });
+        REQUIRE_THAT(pages, AdjDiffMatcher{0});
       }
 
-      THEN("The entries are spaced by PTE_BYTES") {
-        REQUIRE(given_pages.at(1) == given_pages.at(0) + PTE_BYTES);
-        REQUIRE(given_pages.back() == given_pages.front() + (PTE_BYTES*(size-1)));
-      }
-
-      THEN("No pages are duplicated") {
-        auto uniq_end = std::unique(std::begin(given_pages), std::end(given_pages));
-        REQUIRE((std::size_t)std::distance(std::begin(given_pages), uniq_end) == size);
+      THEN("The entries are spaced by pte_page_size") {
+        REQUIRE_THAT(given_pages, AdjDiffMatcher{pte_page_size});
       }
     }
   }
