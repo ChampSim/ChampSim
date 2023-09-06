24de192c2675c879e7788887fbb7a959eeafdf76
Nathan Gober <ngober@protonmail.com>
2023-09-06 11:43:24 -0500

    Update dram_controller.cc

--- src/dram_controller.cc
+++ src/dram_controller.cc
@@ -206,14 +206,14 @@ long DRAM_CHANNEL::schedule_packets()
 
   // Look for queued packets that have not been scheduled
   // prioritize packets that are ready to execute, bank is free
-  auto next_schedule = [&](const auto& lhs, const auto& rhs) {
+  auto next_schedule = [this, &](const auto& lhs, const auto& rhs) {
     if (!(rhs.has_value() && !rhs.value().scheduled))
     return(true);
     if (!(lhs.has_value() && !lhs.value().scheduled))
     return(false);
 
-    auto lop_idx = dram_get_rank(lhs.value().address)*DRAM_BANKS + dram_get_bank(lhs.value().address);
-    auto rop_idx = dram_get_rank(rhs.value().address)*DRAM_BANKS + dram_get_bank(rhs.value().address);
+    auto lop_idx = this->get_rank(lhs.value().address)*this->BANKS + this->get_bank(lhs.value().address);
+    auto rop_idx = this->get_rank(rhs.value().address)*this->BANKS + this->get_bank(rhs.value().address);
     auto rready = !channel.bank_request[rop_idx].valid;
     auto lready = !channel.bank_request[lop_idx].valid;
     return (rready && lready) ? lhs.value().event_cycle < rhs.value().event_cycle : lready;
