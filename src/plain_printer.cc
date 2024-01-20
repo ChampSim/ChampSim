97b65988aa9e5f8aceb586540137563fa801c4de
maccoymerrell <maccoy.merrell@gmail.com>
2024-01-19 18:09:36 -0600

    Change made to plain printer to avoid complications due to switch to time domain.

--- src/plain_printer.cc
+++ src/plain_printer.cc
@@ -88,9 +88,13 @@ void champsim::plain_printer::print(DRAM_CHANNEL::stats_type stats)
   fmt::print(stream, "\n{}\n RQ ROW_BUFFER_HIT: {:12}\n    ROW_BUFFER_MISS: {:11}\n", stats.name, stats.RQ_ROW_BUFFER_HIT, stats.RQ_ROW_BUFFER_MISS);
   fmt::print(stream, " WQ ROW_BUFFER_HIT: {:12}\n    ROW_BUFFER_MISS: {:11}\n    FULL: {:22}\n",stats.WQ_ROW_BUFFER_HIT, stats.WQ_ROW_BUFFER_MISS,
              stats.WQ_FULL);
-  fmt::print(stream, " REFRESHES ISSUED: {:13}\n",stats.refresh_cycles);
+  if(stats.refresh_cycles > 0){
+    fmt::print(stream, " REFRESHES ISSUED: {:13}\n",stats.refresh_cycles);
+  } else {
+    fmt::print(stream, " REFRESHES ISSUED: -\n");
+  }
   if (stats.dbus_count_congested > 0) {
-    fmt::print(stream, " AVG DBUS CONGESTED CYCLE: {:.4g}\n", stats.dbus_cycle_congested / std::ceil(stats.dbus_count_congested));
+    fmt::print(stream, " AVG DBUS CONGESTED CYCLE: {:.4g}\n", (stats.dbus_cycle_congested.count()) / std::ceil(stats.dbus_count_congested));
   } else {
     fmt::print(stream, " AVG DBUS CONGESTED CYCLE: -\n");
   }
