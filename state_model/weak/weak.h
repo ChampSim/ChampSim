90d08c2dc45726fd64395359cf9d1c599ecb374d
GinoC <ginoachacon@gmail.com>
2024-02-01 23:24:26 -0800

    WIP: ↓↓↓↓

--- state_model/weak/weak.h
+++ state_model/weak/weak.h
@@ -5,16 +5,24 @@
 
 #include "cache.h"
 #include "modules.h"
+#include "channel.h"
 
+using request_type = typename champsim::channel::request_type;
+using response_type = typename champsim::channel::response_type;
+
+//Define possible states here
 enum class STATE { MODIFIED, INVALID };
 
 struct weak : champsim::modules::state_model {
   long NUM_SET, NUM_WAY;
-  std::vector<STATE> cache_state;
+  std::vector<STATE> current_state;
   uint64_t cycle = 0;
 
   explicit weak(CACHE* cache);
   weak(CACHE* cache, long sets, long ways);
+
+  bool state_model_handle_request(request_type req);
+  bool state_model_handle_response(response_type resp);
 
 };
 
