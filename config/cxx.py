cbaf28152965f5b522c0828bde8c4ec41b088762
Nathan Gober <ngober@protonmail.com>
2024-02-14 15:19:21 -0600

    refactor: minor modifications to cxx output

--- config/cxx.py
+++ config/cxx.py
@@ -2,6 +2,8 @@ import subprocess
 import tempfile
 import os
 
+from . import util
+
 class CompileResult:
     '''
     The result from check_compiles(), which is convertible to boolean, but retains the output from the compilation check.
@@ -35,6 +37,12 @@ def check_compiles(body, *args, cxx='c++'):
         )
         return CompileResult(result)
 
+def brace_wrap(body):
+    ''' Wrap and indent the iterable. '''
+    yield '{'
+    yield from ('  '+l for l in body)
+    yield '}'
+
 def function(name, body, args=None, rtype=None, qualifiers=tuple()):
     '''
     Yields a C++ function with the given name and body.
@@ -48,10 +56,9 @@ def function(name, body, args=None, rtype=None, qualifiers=tuple()):
     local_args = args or tuple()
     arg_string = ', '.join((a[0]+' '+a[1]) for a in local_args)
     rtype_string = f' -> {rtype}' if rtype is not None else ''
-    yield f'auto {name}({arg_string}){rtype_string}{" ".join(qualifiers)}'
-    yield '{'
-    yield from ('  '+l for l in body)
-    yield '}'
+    qualifier_string = (' '+' '.join(qualifiers)) if qualifiers else ''
+    yield f'auto {name}({arg_string}){rtype_string}{qualifier_string}'
+    yield from brace_wrap(body)
 
 def struct(name, body, superclass=None):
     '''
@@ -63,6 +70,6 @@ def struct(name, body, superclass=None):
     '''
     superclass_string = f' : public {superclass}' if superclass is not None else ''
     yield f'struct {name}{superclass_string}'
-    yield '{'
-    yield from ('  '+l for l in body)
-    yield '};'
+    head, tail = util.cut(brace_wrap(body), n=-1)
+    yield from head
+    yield from (l+';' for l in tail)
