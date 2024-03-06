50028a4eb026cf96927fd063b78e1a2c04fd7284
Nathan Gober <ngober@protonmail.com>
2024-03-06 10:34:39 -0600

    fix: fixed test 121

--- test/cpp/src/121-dib-inorder.cc
+++ test/cpp/src/121-dib-inorder.cc
@@ -19,7 +19,33 @@ struct IsSortedMatcher : Catch::Matchers::MatcherGenericBase {
   }
 };
 
-SCENARIO("Hits to the DIB issue to the ROB in order") {
+auto block_sequence_generator(champsim::block_number blk) {
+  return [blk]() mutable { return champsim::address{blk++}; };
+}
+
+auto instr_id_sequence_generator(uint64_t id) {
+  return [id]() mutable { return id++; };
+}
+
+auto instruction_generator(champsim::block_number base_ip, uint64_t base_id) {
+  return [ip_gen = block_sequence_generator(base_ip), id_gen = instr_id_sequence_generator(base_id)]() mutable {
+    ooo_model_instr instr{champsim::test::instruction_with_ip(ip_gen())};
+    instr.instr_id = id_gen();
+    return instr;
+  };
+}
+
+auto instruction_smem_generator(champsim::block_number base_ip, champsim::block_number base_smem, uint64_t base_id) {
+  return [ip_gen = block_sequence_generator(base_ip), smem_gen = block_sequence_generator(base_smem), id_gen = instr_id_sequence_generator(base_id)]() mutable {
+    ooo_model_instr instr{champsim::test::instruction_with_ip_and_source_memory(ip_gen(), smem_gen())};
+    instr.instr_id = id_gen();
+    return instr;
+  };
+}
+
+SCENARIO("Instructions that hit the DIB do not reorder ahead of instructions that miss") {
+  const std::size_t num_seeds = 3;
+  const std::size_t num_additional_tests = 2;
   GIVEN("A core that has decode a few instructions") {
     do_nothing_MRC mock_L1I, mock_L1D;
 
@@ -30,15 +56,11 @@ SCENARIO("Hits to the DIB issue to the ROB in order") {
     };
     uut.warmup = false;
 
-    const std::size_t num_seeds = 2;
-    const uint64_t seed_base_addr = 0xfeed0040;
+    const champsim::block_number seed_base_addr{0xfeed001};
 
     // Create a sequence of seed instructions
-    std::vector<champsim::address> seed_ips{};
-    std::generate_n(std::back_inserter(seed_ips), num_seeds, [block=champsim::block_number{seed_base_addr}]() mutable { return champsim::address{block++}; });
     std::vector<ooo_model_instr> seed_instructions{};
-    std::transform(std::cbegin(seed_ips), std::cend(seed_ips), std::back_inserter(seed_instructions), [](auto x){ return champsim::test::instruction_with_ip(x); });
-    std::for_each(std::begin(seed_instructions), std::end(seed_instructions), [id = uint64_t{1}](auto& instr) mutable { instr.instr_id = id++; });
+    std::generate_n(std::back_inserter(seed_instructions), num_seeds, instruction_generator(seed_base_addr, 1));
 
     uut.IFETCH_BUFFER.insert(std::end(uut.IFETCH_BUFFER), std::begin(seed_instructions), std::end(seed_instructions));
 
@@ -48,19 +70,12 @@ SCENARIO("Hits to the DIB issue to the ROB in order") {
     }
 
     WHEN("A new instruction is issued, followed by the instructions already seen") {
-      const std::size_t num_tests = num_seeds + 1;
-      const uint64_t test_base_addr = seed_base_addr - BLOCK_SIZE;
-      const uint64_t test_store_base_addr = 0xbeef0000;
-
-      std::vector<champsim::address> test_ips{};
-      std::vector<champsim::address> test_store_addresses_to_check{}; // Use the store buffer to examine the order in which the instructions are retired
-
-      std::generate_n(std::back_inserter(test_ips), num_tests, [block=champsim::block_number{test_base_addr}]() mutable { return champsim::address{block++}; });
-      std::generate_n(std::back_inserter(test_store_addresses_to_check), num_tests, [block=champsim::block_number{test_store_base_addr}]() mutable { return champsim::address{block++}; });
+      const auto num_tests = num_seeds + num_additional_tests;
+      const auto test_base_addr = seed_base_addr - num_additional_tests;
+      const champsim::block_number test_store_base_addr{0xbeef000}; // Use the store buffer to examine the order in which the instructions are retired
 
       std::vector<ooo_model_instr> test_instructions{};
-      std::transform(std::cbegin(test_ips), std::cend(test_ips), std::cbegin(test_store_addresses_to_check), std::back_inserter(test_instructions), champsim::test::instruction_with_ip_and_source_memory);
-      std::for_each(std::begin(test_instructions), std::end(test_instructions), [id = uint64_t{11}](auto& instr) mutable { instr.instr_id = id++; });
+      std::generate_n(std::back_inserter(test_instructions), num_tests, instruction_smem_generator(test_base_addr, test_store_base_addr, 11));
 
       uut.IFETCH_BUFFER.insert(std::end(uut.IFETCH_BUFFER), std::begin(test_instructions), std::end(test_instructions));
 
