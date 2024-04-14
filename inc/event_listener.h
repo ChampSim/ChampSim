2ebf5c3ece82ea808912cd700cba69f82a9909c4
20kholley <20kholley@gmail.com>
2024-04-14 16:53:51 -0500

    Added 7 events for ooocpu

--- inc/event_listener.h
+++ inc/event_listener.h
@@ -11,14 +11,32 @@
 
 #include "champsim.h"
 #include "instruction.h"
+#include "channel.h"
+#include "access_type.h"
 
+#include "ooo_cpu.h"
+//Define here
 enum class event {
   CYCLE_BEGIN,
-  BRANCH
+  BRANCH,
+  DIB,
+  //FETCH,
+  DECODE,
+  RETIRE,
+  EXE,
+  MEM,
+  SQ,
+  CSTORE,
+  ELOAD,
+  VA_TO_PA,
+  GET_PTE_PA,
+  ADD_RQ,
+  ADD_WQ,
+  ADD_PQ
 };
 
 struct CYCLE_BEGIN_data {};
-
+//Add struct here
 struct BRANCH_data {
   ooo_model_instr* instr;
 
@@ -26,6 +44,129 @@ struct BRANCH_data {
     instr = nullptr;
   }
 };
+
+struct DIB_data {
+  ooo_model_instr* instr;
+  long cycle;
+  
+  DIB_data() {
+    instr = nullptr;
+    cycle = 0;
+  }
+};
+/*
+struct FETCH_data {
+  std::deque<ooo_model_instr>::iterator begin;
+  long cycle;
+  
+  FETCH_data() {
+    //begin = nullptr;
+    cycle = 0;
+  }
+};*/
+
+struct DECODE_data {
+  ooo_model_instr* instr;
+  long cycle;
+  
+  DECODE_data() {
+    instr = nullptr;
+    cycle = 0;
+  }
+};
+
+struct RETIRE_data {
+  long cycle;
+  std::vector<ooo_model_instr> instrs;
+  //ooo_model_instr* begin_instr;
+  //ooo_model_instr* end_instr;
+
+  RETIRE_data() {
+    cycle = 0;
+    //begin_instr = nullptr;
+    //end_instr = nullptr;
+  }
+};
+
+struct EXE_data {
+  ooo_model_instr* instr;
+  long cycle;
+  
+  EXE_data() {
+    instr = nullptr;
+    cycle = 0;
+  }
+};
+
+struct MEM_data {
+  ooo_model_instr* instr;
+  long cycle;
+  
+  MEM_data() {
+    instr = nullptr;
+    cycle = 0;
+  }
+};
+
+struct SQ_data {
+  const LSQ_ENTRY* instr;
+  
+  SQ_data() {
+    instr = nullptr;
+  }
+};
+
+struct CSTORE_data {
+  const LSQ_ENTRY* instr;
+  
+  CSTORE_data() {
+    instr = nullptr;
+  }
+};
+
+struct ELOAD_data {
+  const LSQ_ENTRY* instr;
+  
+  ELOAD_data() {
+    instr = nullptr;
+  }
+};
+
+struct VA_TO_PA_data {
+  champsim::page_number paddr;
+  champsim::page_number vaddr;
+  bool fault;
+};
+
+struct GET_PTE_PA_data {
+  champsim::address paddr;
+  champsim::page_number vaddr;
+  uint64_t offset;
+  std::size_t level;
+  bool fault;
+};
+
+struct ADD_RQ_data {
+  uint64_t instr_id;
+  champsim::address address;
+  champsim::address v_address;
+  access_type type;
+};
+
+struct ADD_WQ_data {
+  uint64_t instr_id;
+  champsim::address address;
+  champsim::address v_address;
+  access_type type;
+};
+
+struct ADD_PQ_data {
+  uint64_t instr_id;
+  champsim::address address;
+  champsim::address v_address;
+  access_type type;
+};
+
 
 class EventListener {
 public:
--- src/event_listener.cc
+++ src/event_listener.cc
@@ -22,6 +22,81 @@ void EventListener::process_event(event eventType, void* data) {
     fmt::print("[BRANCH] instr_id: {} ip: {} taken: {}\n", b_data->instr->instr_id, b_data->instr->ip, b_data->instr->branch_taken);
     //fmt::print("Got a branch\n");
   }
+  else if (eventType == event::DIB) {
+    DIB_data* dib_data = static_cast<DIB_data *>(data);
+    fmt::print("[DIB] {} instr_id: {} ip: {} hit: {} cycle: {}\n", __func__, dib_data->instr->instr_id, dib_data->instr->ip, dib_data->instr->fetch_completed,
+               dib_data->cycle);
+    //fmt::print("Got a branch\n");
+  }
+  /*else if (eventType == event::FETCH) {
+    FETCH_data* f_data = static_cast<FETCH_data *>(data);
+    fmt::print("[IFETCH] {} instr_id: {} ip: {:#x} dependents: {} event_cycle: {}\n", __func__, begin->instr_id, begin->ip,
+               std::size(fetch_packet.instr_depend_on_me), cycle);
+    //fmt::print("Got a branch\n");
+  }*/
+  else if (eventType == event::DECODE) {
+    DECODE_data* d_data = static_cast<DECODE_data *>(data);
+    fmt::print("[DECODE] do_decode instr_id: {} cycle: {}\n", d_data->instr->instr_id, d_data->cycle);
+    //fmt::print("Got a branch\n");
+  }
+  else if (eventType == event::EXE) {
+    EXE_data* e_data = static_cast<EXE_data *>(data);
+     fmt::print("[EXE] {} instr_id: {} ready_time: {}\n", __func__, e_data->instr->instr_id, e_data->cycle);
+    //fmt::print("Got a branch\n");
+  }
+  else if (eventType == event::MEM) {
+    MEM_data* m_data = static_cast<MEM_data *>(data);
+     fmt::print("[MEM] {} instr_id: {} loads: {} stores: {} cycle: {}\n", __func__, m_data->instr->instr_id, std::size(m_data->instr->source_memory),
+               std::size(m_data->instr->destination_memory), m_data->cycle);
+    //fmt::print("Got a branch\n");
+  }
+  else if (eventType == event::SQ) {
+    SQ_data* sq_data = static_cast<SQ_data *>(data);
+     fmt::print("[SQ] {} instr_id: {} vaddr: {}\n", __func__, sq_data->instr->instr_id, sq_data->instr->virtual_address);
+    //fmt::print("Got a branch\n");
+  }
+  else if (eventType == event::CSTORE) {
+    CSTORE_data* cs_data = static_cast<CSTORE_data *>(data);
+     fmt::print("[CSTORE] {} instr_id: {} vaddr: {}\n", __func__, cs_data->instr->instr_id, cs_data->instr->virtual_address);
+    //fmt::print("Got a branch\n");
+  }
+  else if (eventType == event::ELOAD) {
+    ELOAD_data* eload_data = static_cast<ELOAD_data *>(data);
+     fmt::print("[EXELOAD] {} instr_id: {} vaddr: {}\n", __func__, eload_data->instr->instr_id, eload_data->instr->virtual_address);
+    //fmt::print("Got a branch\n");
+  }
+  else if (eventType == event::RETIRE) {
+    RETIRE_data* r_data = static_cast<RETIRE_data *>(data);
+    /*fmt::print("[RETIRE] cycle: {}", r_data->cycle);
+    for (auto instr : r_data->instrs) {
+      fmt::print(" instr_id: {}", instr.instr_id);
+    }
+    fmt::print("\n");*/
+    for (auto instr: r_data->instrs) {
+      fmt::print("[ROB] retire_rob instr_id: {} is retired cycle: {}\n", instr.instr_id, r_data->cycle);
+    }
+    /*std::for_each(retire_begin, retire_end, [cycle = current_time.time_since_epoch() / clock_period](const auto& x) {
+      fmt::print("[ROB] retire_rob instr_id: {} is retired cycle: {}\n", x.instr_id, cycle);
+    });*/
+  } else if (eventType == event::VA_TO_PA) {
+    VA_TO_PA_data* v_data = static_cast<VA_TO_PA_data *>(data);
+    fmt::print("[VMEM] va_to_pa paddr: {} vaddr: {} fault: {}\n", v_data->paddr, v_data->vaddr, v_data->fault);
+  } else if (eventType == event::GET_PTE_PA) {
+    GET_PTE_PA_data* g_data = static_cast<GET_PTE_PA_data *>(data);
+    fmt::print("[VMEM] get_pta_pa paddr: {} vaddr: {} pt_page_offset: {} translation_level: {} fault: {}\n", g_data->paddr, g_data->vaddr, g_data->offset, g_data->level, g_data->fault);
+  } else if (eventType == event::ADD_RQ) {
+    ADD_RQ_data* a_data = static_cast<ADD_RQ_data *>(data);
+    fmt::print("[channel_rq] add_rq instr_id: {} address: {} v_address: {} type: {}\n", a_data->instr_id, a_data->address, a_data->v_address,
+               access_type_names.at(champsim::to_underlying(a_data->type)));
+  } else if (eventType == event::ADD_WQ) {
+    ADD_WQ_data* a_data = static_cast<ADD_WQ_data *>(data);
+    fmt::print("[channel_wq] add_wq instr_id: {} address: {} v_address: {} type: {}\n", a_data->instr_id, a_data->address, a_data->v_address,
+               access_type_names.at(champsim::to_underlying(a_data->type)));
+  } else if (eventType == event::ADD_PQ) {
+    ADD_PQ_data* a_data = static_cast<ADD_PQ_data *>(data);
+    fmt::print("[channel_pq] add_pq instr_id: {} address: {} v_address: {} type: {}\n", a_data->instr_id, a_data->address, a_data->v_address,
+               access_type_names.at(champsim::to_underlying(a_data->type)));
+  }
 }
 
 void call_event_listeners(event eventType, void* data) {
