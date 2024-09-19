bb1157254ea3bc52557b27e4bc437326c459c3c2
Quang Duong <duongquang1@gmail.com>
2024-09-18 22:30:05 -0500

    Updated SRRIP find_victim to use same and faster logic as DRRIP

--- replacement/srrip/srrip.cc
+++ replacement/srrip/srrip.cc
@@ -6,25 +6,24 @@
 namespace
 {
 constexpr int maxRRPV = 3;
-std::unordered_map<CACHE*, std::vector<int>> rrpv_values;
+std::unordered_map<CACHE*, std::vector<int>> rrpv;
 } // namespace
 
 // initialize replacement state
-void CACHE::initialize_replacement() { ::rrpv_values[this] = std::vector<int>(NUM_SET * NUM_WAY, ::maxRRPV); }
+void CACHE::initialize_replacement() { ::rrpv[this] = std::vector<int>(NUM_SET * NUM_WAY, ::maxRRPV); }
 
 // find replacement victim
 uint32_t CACHE::find_victim(uint32_t triggering_cpu, uint64_t instr_id, uint32_t set, const BLOCK* current_set, uint64_t ip, uint64_t full_addr, uint32_t type)
 {
   // look for the maxRRPV line
-  auto begin = std::next(std::begin(::rrpv_values[this]), set * NUM_WAY);
+  auto begin = std::next(std::begin(::rrpv[this]), set * NUM_WAY);
   auto end = std::next(begin, NUM_WAY);
-  auto victim = std::find(begin, end, ::maxRRPV); // hijack the lru field
-  while (victim == end) {
-    for (auto it = begin; it != end; ++it)
-      ++(*it);
 
-    victim = std::find(begin, end, ::maxRRPV);
-  }
+  auto victim = std::max_element(begin, end);
+  auto rrpv_update = ::maxRRPV - *victim;
+  if (rrpv_update != 0)
+    for (auto it = begin; it != end; ++it)
+      *it += rrpv_update;
 
   assert(begin <= victim);
   assert(victim < end);
@@ -36,9 +35,9 @@ void CACHE::update_replacement_state(uint32_t triggering_cpu, uint32_t set, uint
                                      uint8_t hit)
 {
   if (hit)
-    ::rrpv_values[this][set * NUM_WAY + way] = 0;
+    ::rrpv[this][set * NUM_WAY + way] = 0;
   else
-    ::rrpv_values[this][set * NUM_WAY + way] = ::maxRRPV - 1;
+    ::rrpv[this][set * NUM_WAY + way] = ::maxRRPV - 1;
 }
 
 // use this function to print out your own stats at the end of simulation
