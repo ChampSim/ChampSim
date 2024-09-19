64d998bba31437c8bc3c70106bfe559040cb54d4
SteliosKaragiorgis <stelioskaragiorgis8@gmail.com>
2024-09-19 20:09:56 +0300

    Remove indirect_branch_missprediction

--- inc/instruction.h
+++ inc/instruction.h
@@ -47,7 +47,6 @@ struct ooo_model_instr {
   bool branch_taken = 0;
   bool branch_prediction = 0;
   bool branch_mispredicted = 0; // A branch can be mispredicted even if the direction prediction is correct when the predicted target is not correct
-  bool indirect_branch_missprediction = 0;
 
   std::array<uint8_t, 2> asid = {std::numeric_limits<uint8_t>::max(), std::numeric_limits<uint8_t>::max()};
 
