90eca1b14f4c5d6818be91deffb2887743e26853
maccoymerrell <maccoy.merrell@gmail.com>
2026-02-13 16:20:10 -0600

    Fixed catch test syntax problem.

--- test/cpp/src/500-environment.cc
+++ test/cpp/src/500-environment.cc
@@ -9,31 +9,32 @@
 TEST_CASE("Module Build Order")
 {
   //given a sequence of cache builders, when we create the environment, the order should be preserved
-  GIVEN("A sequence of cache builders");
+  GIVEN("A sequence of cache builders") {
 
-  auto caches{
-    champsim::configured::build<CACHE>(
-    champsim::cache_builder{ champsim::defaults::default_llc }
-        .name("cache0"),
-    champsim::cache_builder{ champsim::defaults::default_dtlb }
-        .name("cache1"),
-    champsim::cache_builder{ champsim::defaults::default_itlb }
-        .name("cache2"),
-    champsim::cache_builder{ champsim::defaults::default_l1d }
-        .name("cache3")
-    )
+    auto caches{
+        champsim::configured::build<CACHE>(
+        champsim::cache_builder{ champsim::defaults::default_llc }
+            .name("cache0"),
+        champsim::cache_builder{ champsim::defaults::default_dtlb }
+            .name("cache1"),
+        champsim::cache_builder{ champsim::defaults::default_itlb }
+            .name("cache2"),
+        champsim::cache_builder{ champsim::defaults::default_l1d }
+            .name("cache3")
+        )
+    };
+
+    std::vector<std::string> expected_names{"cache0", "cache1", "cache2", "cache3"};
+
+    //require the names match the order of the builders
+    THEN("The caches are built and returned in the same order as the builders") {
+
+        //grab the names of the built caches
+        std::vector<std::string> cache_names{};
+        for (const auto& cache : caches)
+            cache_names.push_back(cache.NAME);
+
+        REQUIRE_THAT(cache_names, Catch::Matchers::Equals(expected_names));
+    }
   };
-
-  std::vector<std::string> expected_names{"cache0", "cache1", "cache2", "cache3"};
-
-  //require the names match the order of the builders
-  THEN("The caches are built and returned in the same order as the builders");
-
-  //grab the names of the built caches
-  std::vector<std::string> cache_names{};
-  for (const auto& cache : caches)
-    cache_names.push_back(cache.NAME);
-
-  REQUIRE(cache_names == expected_names);
-
 }
